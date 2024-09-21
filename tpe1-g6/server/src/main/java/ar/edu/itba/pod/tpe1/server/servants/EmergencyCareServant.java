e310b77c943536a4b94698431b184f07c8a0a6d5
Matias Ricarte <mricarte@itba.edu.ar>
2024-09-21 19:46:46 -0300

    query wip + history repo

--- tpe1-g6/server/src/main/java/ar/edu/itba/pod/tpe1/server/servants/EmergencyCareServant.java
+++ tpe1-g6/server/src/main/java/ar/edu/itba/pod/tpe1/server/servants/EmergencyCareServant.java
@@ -6,6 +6,10 @@ import ar.edu.itba.pod.tpe1.emergencyCare.CarePatientResponse;
 import ar.edu.itba.pod.tpe1.emergencyCare.DischargePatientRequest;
 import ar.edu.itba.pod.tpe1.emergencyCare.DischargePatientResponse;
 import ar.edu.itba.pod.tpe1.emergencyCare.EmergencyCareServiceGrpc;
+import ar.edu.itba.pod.tpe1.server.repository.DoctorsRepository;
+import ar.edu.itba.pod.tpe1.server.repository.HistoryRepository;
+import ar.edu.itba.pod.tpe1.server.repository.PatientsRepository;
+import ar.edu.itba.pod.tpe1.server.repository.RoomsRepository;
 import com.google.protobuf.Empty;
 import io.grpc.stub.StreamObserver;
 import org.slf4j.Logger;
@@ -13,6 +17,20 @@ import org.slf4j.LoggerFactory;
 
 public class EmergencyCareServant extends EmergencyCareServiceGrpc.EmergencyCareServiceImplBase {
     private static final Logger logger = LoggerFactory.getLogger(EmergencyCareServant.class);
+
+    private final PatientsRepository patientsRepository;
+    private final RoomsRepository roomsRepository;
+    private final DoctorsRepository doctorsRepository;
+    private final HistoryRepository historyRepository;
+
+
+    public EmergencyCareServant(PatientsRepository pR, DoctorsRepository dR, RoomsRepository rR, HistoryRepository hR) {
+        patientsRepository = pR;
+        doctorsRepository = dR;
+        roomsRepository = rR;
+        historyRepository = hR;
+    }
+
 
     public void carePatient(CarePatientRequest request, StreamObserver<CarePatientResponse> responseObserver) {
         logger.info("Attending patient ...");
@@ -24,5 +42,7 @@ public class EmergencyCareServant extends EmergencyCareServiceGrpc.EmergencyCare
 
     public void dischargePatient(DischargePatientRequest request, StreamObserver<DischargePatientResponse> responseObserver) {
         logger.info("Discharging patient ...");
+        //TODO: implement
+        //remember to add to historyRepository! it's supposed to store completed cares
     }
 }
--- tpe1-g6/server/src/main/java/ar/edu/itba/pod/tpe1/server/servants/QueryServant.java
+++ tpe1-g6/server/src/main/java/ar/edu/itba/pod/tpe1/server/servants/QueryServant.java
@@ -1,10 +1,43 @@
 package ar.edu.itba.pod.tpe1.server.servants;
 
-import ar.edu.itba.pod.tpe1.query.QueryServiceGrpc;
+import ar.edu.itba.pod.tpe1.query.*;
+import ar.edu.itba.pod.tpe1.server.repository.HistoryRepository;
+import com.google.protobuf.Empty;
+import io.grpc.stub.StreamObserver;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+
+import java.util.List;
+import java.util.Optional;
 
 public class QueryServant extends QueryServiceGrpc.QueryServiceImplBase {
     private static final Logger logger = LoggerFactory.getLogger(QueryServant.class);
 
+    private final HistoryRepository historyRepository;
+
+    public QueryServant(HistoryRepository hR) {
+        historyRepository = hR;
+    }
+
+
+    public void QueryRooms(Empty request, StreamObserver<QueryRoomsResponse> responseObserver) {
+        //TODO: implement
+    }
+
+    public void QueryWaitingRoom(Empty request, StreamObserver<QueryWaitingRoomResponse> responseObserver) {
+        //TODO: implement
+    }
+
+    public void QueryCares(QueryRequest request, StreamObserver<QueryCaresResponse> responseObserver) {
+        List<CaredInfo> history = request.hasRoom() ?
+                historyRepository.getHistory(request.getRoom())
+                : historyRepository.getHistory();
+
+        QueryCaresResponse response = QueryCaresResponse.newBuilder()
+                .addAllHistory(history)
+                .build();
+
+        responseObserver.onNext(response);
+        responseObserver.onCompleted();
+    }
 }
